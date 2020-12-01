e9587219a4dd72e3f9e45bbd87c011b78b016dbf
Vadim Zaripov <vadim@zaripov.website>
2020-12-01 01:25:52 +0100

    Initial commit

---
+++ src/test/java/zaripov/website/airlineticketsystem/AirlineticketsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package zaripov.website.airlineticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineticketsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
